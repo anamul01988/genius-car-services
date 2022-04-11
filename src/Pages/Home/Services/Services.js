4441f204b6340087ea3dc0a3d86d9accab4f92ba
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-11 22:53:39 +0600

    carousel added with react-bootstrap

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -11,7 +11,7 @@ const Services = () => {
     },[])
     return (
         <div >
-            <h1 className='services-title'>Our Services</h1>
+            <h1 className='services-title mt-5'>Our Services</h1>
             <p>services: { services.length}</p>
            <div className="services-container">
            {
